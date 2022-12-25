0eb3521c8e93928caf8a50c8fba614c205864496
sara-mostafa-sabry <sara.mostafa.sabry@hotmail.com>
2022-12-25 14:13:55 +0200

    add lib and config f JTE

---
+++ libraries/maven/steps/build.groovy
@@ -0,0 +1,14 @@
+void call(){
+    stage("Maven: Build"){
+
+            steps {
+
+                  sh """
+                       java -version
+                       mvn -version
+                       ./mvnw package
+                     """
+
+                }
+
+        }
